12073ff097913376b423308d5a7f9b48597bdde4
Seb <seb_in_hogsmead@yahoo.ca>
2018-09-16 02:27:29 -0400

    Finished retrieving data for alternate-view-articles

--- find_related.py
+++ find_related.py
@@ -1,6 +1,32 @@
 import urllib.request
+import datetime
+from time import sleep
 
 from bs4 import BeautifulSoup
+
+def _relevent_date_star(article_info, article_date):
+    seconds_per_3_weeks = datetime.timedelta(seconds=604800)
+    url = article_info["url"]
+
+    soup = BeautifulSoup(urllib.request.urlopen(url), 'html.parser')
+    alt_article_date = soup.find_all("meta", {"name": "parsely-pub-date"})[0]['content']
+    alt_article_date = datetime.datetime.strptime( alt_article_date, "%Y-%m-%dT%H:%M:%S.%fZ" )
+    if (article_date - alt_article_date) >= seconds_per_3_weeks:
+        return True #TODO: fix dattime
+    return True
+
+def _relevent_date_sun(article_info, article_date):
+    seconds_per_3_weeks = datetime.timedelta(seconds=604800)
+    url = article_info["url"]
+
+    soup = BeautifulSoup(urllib.request.urlopen(url), 'html.parser')
+    alt_article_date = soup.find_all("time", {"class": "updated"})[0]['datetime'][0:-6]
+
+    alt_article_date = datetime.datetime.strptime( alt_article_date, "%Y-%m-%dT%H:%M:%S" )
+    if (article_date - alt_article_date) >= seconds_per_3_weeks:
+        return True #TODO: fix dattime
+    return True
+
 
 
 def _get_search_html(domain_name, keywords, keyword_separator):
@@ -18,37 +44,45 @@ def _get_search_html(domain_name, keywords, keyword_separator):
     return BeautifulSoup(urllib.request.urlopen(url), 'html.parser')
 
 
-def get_article_urls(keywords, site):
-    article_urls = {}
+def find_related_articles(keywords, site):
+    article_data = []
 
-    if site == "toronto star":
+    if site == "thestar":
         first_html_child = 0
-        soup = _get_search_html("https://www.thestar.com/search.html?q=", keywords, "%20")
+        soup = _get_search_html("https://www.thestar.com/search.html?sortby=relevance&sortdirection=desc&q=", keywords, "%20")
 
         # extract stories to crawl
-        for article in soup.find_all("div", {"class": "story__body"}):
-            article_urls["https://www.thestar.com" + article
-                .contents[first_html_child].contents[first_html_child]
-                .contents[first_html_child].get('href')] = 0
+        for article in soup.find_all("div", {"class": "story"}):
+            url = "https://www.thestar.com" + article.find_all("a")[0]["href"]
+            print(article)
+            try:
+                img_url = "https://www.thestar.com" + article.find_all("img")[0]["src"]
+            except:
+                img_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM8uGLeO03VvFX8zhIAQMxpZCNL9K5w1bgDurygXMQknA8qcw5RQ"
+            title = article.find_all("a")[1].get_text()
+            article_data.append({"url": url, "img_url": img_url, "title": title})
 
-    elif site == "toronto sun":
+
+        article_data = filter( lambda x : _relevent_date_star(x,datetime.datetime.now()) , article_data)
+
+    elif site == "torontosun":
         soup = _get_search_html("https://torontosun.com/?s=", keywords, "+")
 
         # extract stories to crawl
         for article in soup.find_all('article'):
-            article_urls[article.contents[1].contents[1].get('href')] = 0
+            url = article.contents[1].contents[1].get('href')
+            img_url = article.contents[1].contents[1].contents[1]["src"]
+            title = article.find_all("h4")[0].get_text()
+            article_data.append({"url": url, "img_url": img_url, "title": title})
+
+        article_data = filter( lambda x : _relevent_date_sun(x,datetime.datetime.now()) , article_data)
+
 
     else:
         print("error: this website is not supported")
         exit(1)
 
-    return article_urls
-
-
-def find_related_articles(keywords, site):
-    article_urls = get_article_urls(keywords, site)
-    print (article_urls)
-
+    return article_data
 
 if __name__== '__main__':
     find_related_articles(["climate", "change"], "toronto star")
--- news_pipline.py
+++ news_pipline.py
@@ -1,11 +1,13 @@
 ####################################################################################
 # Main Function to run backend script after user init the plugin
 ####################################################################################
-import news_scrape from news_scraper
+from news_scraper import news_scrape
+from find_related import find_related_articles
 
 def news_pipline(url):
     num_keyword = 3
-    news_scrape(url, num_keyword)
-    # sebasian's function
-    # get opposing ling
-    # ....
+    data = news_scrape(url, num_keyword)
+    find_related_articles(data[1], data[0])
+
+if __name__ == "__main__":
+    news_pipline("thestar")
